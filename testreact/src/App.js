49fd06c06be248bb75ccd70e890f69add17a1992
Chars4785 <9105lgm@naver.com>
2021-12-10 10:24:22 +0900

    config

--- testreact/src/App.js
+++ testreact/src/App.js
@@ -1,17 +1,48 @@
-import logo from './logo.svg';
+import react, { useEffect } from "react"
 import './App.css';
-import useSWR from 'swr'
+import useSWR, { SWRConfig, useSWRConfig } from 'swr'
 import axios from 'axios';
 
 const fetcher = url => axios.get(url).then(res => res.data)
 
-function App() {
-  const { data, error } = useSWR('http://localhost:3333',fetcher)
-  console.log(data,error)
-  return (
-    <div className="App">
-      {data}    
+function Dash(){
+  const result = useSWR('http://localhost:3333')
+  const { data, error, isValidating} = result
+  const config = useSWRConfig()
+  const { refreshInterval, mutate, cache, ...restConfig } = config
+  useEffect(()=>{
+    console.log("in useEffect")
+  },[])
+  return(
+    <div>
     </div>
+  )
+}
+
+function Tool(){
+  const result = useSWR('http://localhost:3333')
+  const { data, error, isValidating, mutate } = result
+  return(
+    <div>
+      {/* {data} */}
+    </div>
+  )
+}
+
+function App() {
+  // const { data, error, isValidating, mutate } = useSWR('http://localhost:3333',fetcher)
+  // const { data, error, isValidating, mutate } = useSWR(key, fetcher, options)
+  return (
+    <SWRConfig value={{
+      refreshInterval:3000,
+      fetcher,
+      provider:() => new Map()
+    }}>
+      <div className="App">
+        <Dash />
+        <Tool />
+      </div>
+    </SWRConfig>
   );
 }
 
